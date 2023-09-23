482a5fa992d1fbeb3f3cdd786de87a6ae5138d40
gaurav bhasker <gauravbhasker8@gmail.com>
2023-09-23 13:12:24 +0530

    Add files via upload

---
+++ service-discovery/src/test/java/com/odin/orchestrator/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odin.orchestrator.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
